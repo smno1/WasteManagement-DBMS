67cd9272b75cdb009e17f22d50609aaaaaede850
sheen <ysmsmno1@gmail.com>
2015-01-31 16:43:15 +1100

    fix invalid variable

--- app/models/service.rb
+++ app/models/service.rb
@@ -33,7 +33,7 @@ class Service < ActiveRecord::Base
   
   after_destroy do |sv|
     unless Site.find_by_id(sv.site_id).blank?
-      site_sab=Site.find_by_id(sid).saving_against_baselines.group(:month)
+      site_sab=Site.find_by_id(sv.site_id).saving_against_baselines.group(:month)
       site_sab.each do |sab|
         SiteMonthSaving.update_when_saving_against_baseline_changed(sv.site_id,sab.month)
       end
